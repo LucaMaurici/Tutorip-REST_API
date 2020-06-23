48e11edff66abd2e437f372fe14385bbeb85b032
LucaMaurici <57830854+LucaMaurici@users.noreply.github.com>
2020-06-23 16:42:40 +0200

    Aggiunto findMaterieByIdInsegnante

---
+++ TutoripREST/materia/findMaterieByIdInsegnante.php
@@ -0,0 +1,46 @@
+<?php
+//headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../config/database.php';
+include_once '../models/materia.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$o = new materia($db);
+
+$data = json_decode(file_get_contents("php://input"));
+$o->id = $data->id;
+$stmt = $o->findByIdInsegnante();
+$num = $stmt->rowCount();
+
+if($num>0){
+	$arr = array();
+	$arr['ElencoMaterie'] = array();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+
+        $item = array(
+            "nome" => $nome
+        );
+
+        array_push($arr['ElencoMaterie'], $item);
+    }
+
+    http_response_code(200);
+    echo json_encode($arr);
+}else{
+
+    http_response_code(404);
+
+    echo json_encode(
+        array("message" => "Nessuna materia trovata.")
+    );
+}
+?>
---
+++ TutoripREST/models/materia.php
@@ -0,0 +1,38 @@
+<?php
+include_once "../Service/parameters.php";
+
+class Materia
+{
+	private $conn;
+    
+	// proprietà
+    public $id;
+
+	// costruttore
+	public function __construct($db)
+	{
+		$this->conn = $db;
+	}
+	
+	// findByid
+    function findByIdInsegnante(){
+		
+        $query = "	SELECT m.nome, m.prioritàVisualizzazione
+                  	FROM
+						Insegnanti_Materie im JOIN Materie m ON im.cod_materia = m.id
+      				WHERE im.cod_insegnante = :id
+					ORDER BY m.prioritàVisualizzazione DESC";
+                    
+        $stmt = $this->conn->prepare($query);
+        
+		$stmt->bindParam(":id", prepareParam($this->id));
+		
+		//echo prepareParam($this->id);
+		
+        // execute query
+		$stmt->execute();
+		return $stmt;
+    }
+	
+}
+?>
