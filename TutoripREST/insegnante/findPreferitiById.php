cfa8624f7b8d40fd248fb8696233544c13dc2078
a-vitale <63303554+a-vitale@users.noreply.github.com>
2020-06-24 11:26:53 +0200

    FindPreferitiById (json)
    File di creazione del json

---
+++ TutoripREST/insegnante/findPreferitiById.php
@@ -0,0 +1,36 @@
+<?php
+//headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../config/database.php';
+include_once '../models/insegnante.php';  
+
+$o = new insegnate($db);
+$data = json_decode(file_get_contents("php://input"));
+$o->id = $data->id;
+$stmt = $o->findPreferitiById();
+$num = $stmt->rowCount();
+
+if($num>0){
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+	    $item = array(
+             "id" => $id,
+			 "nomeDaVisualizzare" => $nomeDaVisualizzare,
+			 "tariffa" => $tariffa,
+			 "valutazioneMedia" => $valutazioneMedia,
+		);
+	}
+	http_response_code(200);
+    echo json_encode($item);
+}else{
+	http_response_code(404);
+	echo json_encode(
+        array("message" => "Nessun insegnante trovato.")
+    );
+}
+?>
